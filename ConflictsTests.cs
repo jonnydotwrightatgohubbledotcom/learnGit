46cc9806d2826081ca7da7588e629f37016c29e6
INSIGHTSOFTWARE\jonny <jonny.wright@insightsoftware.com>
2016-10-25 16:48:59 +0100

    Grammar fix

--- ConflictsTests.cs
+++ ConflictsTests.cs
@@ -5,7 +5,7 @@ namespace GitTests {
 	public class MyClass {
 		
 		public static void Sum(int[] numbers) {
-			Console.WriteLine("Summing with {0} numbers.", numbers.Count);
+			Console.WriteLine("Summing with {0} number(s).", numbers.Count);
 			Console.WriteLine("Called from within MyClass.");
 			
 			int result = 0;
