de8fdc46b4f7584c6bd76e98c9083821c4c3cf41
INSIGHTSOFTWARE\jonny <jonny.wright@insightsoftware.com>
2016-10-25 16:46:25 +0100

    Pull loggin messages out to logging method

--- ConflictsTests.cs
+++ ConflictsTests.cs
@@ -4,16 +4,22 @@ namespace GitTests {
 	
 	public class MyClass {
 		
-		public static void Sum(int[] numbers) {
-			Console.WriteLine("Summing with {0} numbers.", numbers.Count);
-			Console.WriteLine("Called from within MyClass.");
-			
+		public static void Sum(int[] numbers)
+		{
+			LogMessages();
+
 			int result = 0;
 			foreach (int number in numbers) {
 				result += number;
 			}
 			return result;
 		}
-		
+
+
+		private static void LogMessages()
+		{
+			Console.WriteLine("Summing with {0} numbers.", numbers.Count);
+			Console.WriteLine("Called from within MyClass.");
+		}
 	}
 }
